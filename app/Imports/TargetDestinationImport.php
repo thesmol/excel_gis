814bf0e809e97246bf19dbfca5b41f636f89e6ce
Vanya <ranzar2000@gmail.com>
2022-06-29 00:56:16 +0300

    Create import class for target_destinations

--- app/Imports/TargetDestinationImport.php
+++ app/Imports/TargetDestinationImport.php
@@ -3,19 +3,64 @@
 namespace App\Imports;
 
 use App\Models\target_destination;
+use Illuminate\Validation\Rule;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use Maatwebsite\Excel\Concerns\WithValidation;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\SkipsFailures;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
+use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
 
-class TargetDestinationImport implements ToModel
+class TargetDestinationImport implements WithMultipleSheets
+{
+
+    public function sheets(): array
+    {
+        return [
+            'Лицензия' => new SheetImport(),
+        ];
+    }
+}
+
+
+class SheetImport implements ToModel, WithHeadingRow, WithValidation, SkipsEmptyRows, SkipsOnFailure
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
+
+    use Importable, SkipsFailures;
+
     public function model(array $row)
     {
         return new target_destination([
-            //
+            'target' => $row['target_destination']
         ]);
     }
+
+    public function chunkSize(): int
+    {
+        return 5000;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'target' => Rule::unique('target_destination', 'target'), // Table name, field in your db
+        ];
+    }
+
+    public function customValidationMessages()
+    {
+        return [
+            'target.unique' => 'Duplicate',
+        ];
+    }
 }
+
+
+
