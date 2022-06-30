b5c1741f26e467bde2558077e7a5e7a16114048d
Vanya <ranzar2000@gmail.com>
2022-06-30 14:25:34 +0300

    Delete unknown something

--- app/Models/license_status.php
+++ app/Models/license_status.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class license_status extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'ls_id';
+    protected $fillable = ['status'];
+    public $timestamps = false;
+    public $table = 'license_statuses';
 }
