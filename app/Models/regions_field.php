f0e6f8dbd5a5f17289f2134e756c81cd96f02ae7
Vanya <ranzar2000@gmail.com>
2022-07-06 23:22:03 +0300

    Create import for regions fields

--- app/Models/regions_field.php
+++ app/Models/regions_field.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class regions_field extends Model
 {
     use HasFactory;
 
-    protected $primaryKey = ['fields_id', 'region_rves_id'];
+    protected $primaryKey = 'rf_id';
     protected $fillable = ['fields_id', 'region_rves_id'];
     public $timestamps = false;
     public $table = 'regions_fields';
