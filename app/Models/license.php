023a3213c2beaaad704857c80186e393847303b4
Vanya <ranzar2000@gmail.com>
2022-07-07 21:13:42 +0300

    Add validation rules and fill fillable columns in model license

--- app/Models/license.php
+++ app/Models/license.php
@@ -9,7 +9,29 @@ class license extends Model
 {
     use HasFactory;
 
-    protected $primaryKey = '*_id';
-    protected $fillable = [''];
+    protected $primaryKey = 'l_id';
+    protected $fillable = [
+        'prev_l_id',
+        'l_series',
+        'l_number',
+        'l_type',
+        'company_id',
+        'license_area_id',
+        'license_status_id',
+        'target_destination',
+        'kind_of_fossil',
+        'authorities_id',
+        'date_of_start',
+        'date_of_end',
+        'date_of_annulation',
+        'coords',
+        ];
     public $timestamps = false;
+    public $table = 'licenses';
+    static function rules(): array {
+		return [
+			'company_id' => 'required|integer|exists:company, c_id',
+
+		];
+	}
 }
